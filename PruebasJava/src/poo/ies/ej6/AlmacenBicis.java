47accbbc39eacec207b419e7e11a79610a959ad7
AORUS <oscarmartinez76@hotmail.es>
2024-01-31 12:06:02 +0100

    Ejercicio terminado

--- PruebasJava/src/poo/ies/ej6/AlmacenBicis.java
+++ PruebasJava/src/poo/ies/ej6/AlmacenBicis.java
@@ -3,26 +3,20 @@ package poo.ies.ej6;
 import java.util.Scanner;
 public class AlmacenBicis {
     Scanner leer=new Scanner(System.in);
-    static int stockTotal = 0; //Contador
-    final int MAX_BICICLETAS = 10;
-   public static Bicicleta[] bicicletas;
-    int opcion;
-
     int referencia;
     String marca;
     String modelo;
+    int opcion;
+    static int stockTotal = 0; //Contador
+    final int MAX_BICICLETAS = 10;
+    public static Bicicleta[] bicicletas;
 
 
-    public AlmacenBicis(int numBicis){
-
-        bicicletas = new Bicicleta[numBicis];
+    public AlmacenBicis() {
+        bicicletas = new Bicicleta[MAX_BICICLETAS];
     }
 
-    public Bicicleta[] getBicicletas() {
-        return bicicletas;
-    }
-
-    public void agregarBicicletas(Bicicleta[] nuevasBicicletas) {
+    public void agregarNuevasBicicletas(Bicicleta[] nuevasBicicletas) {
         for (Bicicleta nuevaBicicleta : nuevasBicicletas) {
             if (stockTotal < MAX_BICICLETAS && nuevaBicicleta != null) {
                 bicicletas[stockTotal] = nuevaBicicleta;
@@ -33,45 +27,45 @@ public class AlmacenBicis {
         }
     }
 
-    public void addBicicleta() {
-        System.out.print("Introduce la referencia de la bicicleta: ");
-        int referencia = leer.nextInt();
+    public void agregarBicicleta() {
+        if (stockTotal < MAX_BICICLETAS) {
+            System.out.print("Introduce la referencia de la bicicleta: ");
+            int referencia = leer.nextInt();
 
-        if (!existeBicicletaConReferencia(referencia)) {
-            Bicicleta nuevaBicicleta = new Bicicleta();
-            nuevaBicicleta.setReferencia(referencia);
-            System.out.print("Introduce la marca de la bicicleta: ");
-            nuevaBicicleta.setMarca(leer.next());
-            System.out.print("introduce el modelo de la bici: ");
-            nuevaBicicleta.setModelo(leer.next());
-            System.out.print("Introduce el peso de la bici:");
-            nuevaBicicleta.setPeso(leer.nextFloat());
-            System.out.print("Introduce el tamaño de las ruedas de la bici. ");
-            nuevaBicicleta.setTamanyo(leer.nextFloat());
+            if (!existeBicicletaConReferencia(referencia)) {
+                Bicicleta nuevaBicicleta = new Bicicleta();
+                nuevaBicicleta.setReferencia(referencia);
+                System.out.print("Introduce la marca de la bicicleta: ");
+                nuevaBicicleta.setMarca(leer.next());
+                System.out.print("Introduce el modelo de la bici: ");
+                nuevaBicicleta.setModelo(leer.next());
+                System.out.print("Introduce el peso de la bici: ");
+                nuevaBicicleta.setPeso(leer.nextFloat());
+                System.out.print("Introduce el tamaño de las ruedas de la bici. ");
+                nuevaBicicleta.setTamanyo(leer.nextFloat());
 
-            System.out.print("Introduce si tiene motor (si/no): ");
-            String tieneMotor = leer.next();
+                System.out.print("Introduce si tiene motor (si/no): ");
+                String tieneMotor = leer.next();
 
-            boolean MotorSi = obtenerMotor(tieneMotor);
-            nuevaBicicleta.setMotor(MotorSi);
+                boolean motorSi = obtenerMotor(tieneMotor);
+                nuevaBicicleta.setMotor(motorSi);
 
-            System.out.println("Introduce la fecha de fabricación (DD/MM/AAAA): " );
-            nuevaBicicleta.setFechaFabricacion(leer.next());
-            System.out.print("Introduce el precio :");
-            nuevaBicicleta.setPrecio(leer.nextFloat());
+                System.out.println("Introduce la fecha de fabricación (DD/MM/AAAA): ");
+                nuevaBicicleta.setFechaFabricacion(leer.next());
+                System.out.print("Introduce el precio: ");
+                nuevaBicicleta.setPrecio(leer.nextFloat());
 
-
-            if (stockTotal < MAX_BICICLETAS) {
                 bicicletas[stockTotal] = nuevaBicicleta;
                 stockTotal++;
                 imprimirTabla(bicicletas);
 
             } else {
-                System.out.println("Error: No hay espacio disponible para agregar más bicicletas.");
+                System.out.println("Error: Ya existe una bicicleta con esa referencia.");
             }
+        } else {
+            System.out.println("Error: No hay espacio disponible para agregar más bicicletas.");
         }
     }
-
     public void ventaBici(){
         System.out.print("Ingrese la referencia de venta: ");
         referencia = leer.nextInt();
@@ -83,9 +77,7 @@ public class AlmacenBicis {
                 imprimirTabla(bicicletas);
             }
         }
-
     }
-
     public void consultaBici(){
         do {
             System.out.println("***********************\n" +
@@ -101,9 +93,9 @@ public class AlmacenBicis {
                      System.out.print("Ingrese una referencia: ");
                      referencia = leer.nextInt();
                      // Imprime la cabecera de la tabla
-                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+-------+");
-                     System.out.println("| Referencia |  Marca  |  Modelo  | Peso Kg |T. Ruedas| Motor | Fecha fabr. | Precio | Stock |");
-                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+-------+");
+                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+");
+                     System.out.println("| Referencia |  Marca  |  Modelo  | Peso Kg |T. Ruedas| Motor | Fecha fabr. | Precio |");
+                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+");
 
                      // Itera sobre las bicicletas e imprime solo las que coinciden con la marca
                      for (int i = 0; i < stockTotal; i++) {
@@ -112,11 +104,11 @@ public class AlmacenBicis {
                              System.out.printf("|%-12d|%-9s|%-10s|%-9.2f|%-9.2f|%-7s|%-13s|%-8.2f|%-7d|\n",
                                      refBicicleta.getReferencia(), refBicicleta.getMarca(), refBicicleta.getModelo(),
                                      refBicicleta.getPeso(), refBicicleta.getTamanyo(), refBicicleta.isMotor(),
-                                     refBicicleta.getFechaFabricacion(), refBicicleta.getPrecio(), stockTotal);
+                                     refBicicleta.getFechaFabricacion(), refBicicleta.getPrecio());
                          }
                      }
                      // Imprime el pie de la tabla
-                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+-------+");
+                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+");
                      break;
 
 
@@ -124,9 +116,9 @@ public class AlmacenBicis {
                      System.out.print("Ingrese una marca: ");
                      marca = leer.next();
                      // Imprime la cabecera de la tabla
-                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+-------+");
-                     System.out.println("| Referencia |  Marca  |  Modelo  | Peso Kg |T. Ruedas| Motor | Fecha fabr. | Precio | Stock |");
-                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+-------+");
+                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+");
+                     System.out.println("| Referencia |  Marca  |  Modelo  | Peso Kg |T. Ruedas| Motor | Fecha fabr. | Precio |");
+                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+");
 
                      // Itera sobre las bicicletas e imprime solo las que coinciden con la marca
                      for (int i = 0; i < stockTotal; i++) {
@@ -135,20 +127,20 @@ public class AlmacenBicis {
                                  System.out.printf("|%-12d|%-9s|%-10s|%-9.2f|%-9.2f|%-7s|%-13s|%-8.2f|%-7d|\n",
                                          marcaBicicleta.getReferencia(), marcaBicicleta.getMarca(), marcaBicicleta.getModelo(),
                                          marcaBicicleta.getPeso(), marcaBicicleta.getTamanyo(), marcaBicicleta.isMotor(),
-                                         marcaBicicleta.getFechaFabricacion(), marcaBicicleta.getPrecio(), stockTotal);
+                                         marcaBicicleta.getFechaFabricacion(), marcaBicicleta.getPrecio());
                              }
                      }
                      // Imprime el pie de la tabla
-                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+-------+");
+                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+");
                      break;
 
                  case 3:
                      System.out.print("Ingrese un modelo: ");
                      modelo = leer.next();
                      // Imprime la cabecera de la tabla
-                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+-------+");
-                     System.out.println("| Referencia |  Marca  |  Modelo  | Peso Kg |T. Ruedas| Motor | Fecha fabr. | Precio | Stock |");
-                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+-------+");
+                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+");
+                     System.out.println("| Referencia |  Marca  |  Modelo  | Peso Kg |T. Ruedas| Motor | Fecha fabr. | Precio |");
+                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+");
 
                      // Itera sobre las bicicletas e imprime solo las que coinciden con la marca
                      for (int i = 0; i < stockTotal; i++) {
@@ -157,11 +149,11 @@ public class AlmacenBicis {
                                  System.out.printf("|%-12d|%-9s|%-10s|%-9.2f|%-9.2f|%-7s|%-13s|%-8.2f|%-7d|\n",
                                          modeloBicicleta.getReferencia(), modeloBicicleta.getMarca(), modeloBicicleta.getModelo(),
                                          modeloBicicleta.getPeso(), modeloBicicleta.getTamanyo(), modeloBicicleta.isMotor(),
-                                         modeloBicicleta.getFechaFabricacion(), modeloBicicleta.getPrecio(), stockTotal);
+                                         modeloBicicleta.getFechaFabricacion(), modeloBicicleta.getPrecio());
                              }
                      }
                      // Imprime el pie de la tabla
-                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+-------+");
+                     System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+");
                      break;
              }
         }while (opcion !=0);
@@ -186,22 +178,19 @@ public class AlmacenBicis {
 
     public static void imprimirTabla(Bicicleta[] bicicletas) {
         // Método imprime la tabla, no es necesario tener una instancia de bicicletas
-        System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+-------+");
-        System.out.println("| Referencia |  Marca  |  Modelo  | Peso Kg |T. Ruedas| Motor | Fecha fabr. | Precio | Stock |");
-        System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+-------+");
+        System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+");
+        System.out.println("| Referencia |  Marca  |  Modelo  | Peso Kg |T. Ruedas| Motor | Fecha fabr. | Precio |");
+        System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+");
 
         for (int i = 0; i < stockTotal; i++) {
-            Bicicleta currentBicicleta = bicicletas[i];
-            if (currentBicicleta != null) {
+            Bicicleta todasLasBicicletas = bicicletas[i];
+            if (todasLasBicicletas != null) {
                 System.out.printf("|%-12d|%-9s|%-10s|%-9.2f|%-9.2f|%-7s|%-13s|%-8.2f|%-7d|\n",
-                        currentBicicleta.getReferencia(), currentBicicleta.getMarca(), currentBicicleta.getModelo(),
-                        currentBicicleta.getPeso(), currentBicicleta.getTamanyo(), currentBicicleta.isMotor(),
-                        currentBicicleta.getFechaFabricacion(), currentBicicleta.getPrecio(), stockTotal);
+                        todasLasBicicletas.getReferencia(), todasLasBicicletas.getMarca(), todasLasBicicletas.getModelo(),
+                        todasLasBicicletas.getPeso(), todasLasBicicletas.getTamanyo(), todasLasBicicletas.isMotor(),
+                        todasLasBicicletas.getFechaFabricacion(), todasLasBicicletas.getPrecio());
             }
         }
-
-        System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+-------+");
+        System.out.println("+------------+---------+----------+---------+---------+-------+-------------+--------+");
     }
-
-
 }
--- PruebasJava/src/poo/ies/ej6/Tienda.java
+++ PruebasJava/src/poo/ies/ej6/Tienda.java
@@ -2,26 +2,25 @@ package poo.ies.ej6;
 
 import java.util.Scanner;
 
-import static poo.ies.ej6.Bicicleta.*;
+import static poo.ies.ej6.AlmacenBicis.bicicletas;
 
 public class Tienda {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
-        AlmacenBicis almacen = new AlmacenBicis(10);
+        AlmacenBicis almacen = new AlmacenBicis();
 
-        Bicicleta[] bici=new Bicicleta[10];
-
-        bici[0] = new Bicicleta(1, "HP", "mod", 6, 12, true, "21/05/2006", 1500);
-        bici[1] = new Bicicleta(2, "BH", "XM", 3, 17, true, "12/09/2009", 1700);
-        bici[2] = new Bicicleta(3, "BH", "Road", 2, 16, false, "30/08/2001", 900);
-        bici[3] = new Bicicleta(4, "CR", "pepino", 4, 14, false, "18/01/2004", 700);
-        int opcion;
+        Bicicleta[] nuevaBicicleta=new Bicicleta[4];
+        nuevaBicicleta[0] = new Bicicleta(1, "HP", "mod", 6, 12, true, "21/05/2006", 1500);
+        nuevaBicicleta[1] = new Bicicleta(2, "BH", "XM", 3, 17, true, "12/09/2009", 1700);
+        nuevaBicicleta[2] = new Bicicleta(3, "BH", "Road", 2, 16, false, "30/08/2001", 900);
+        nuevaBicicleta[3] = new Bicicleta(4, "CR", "pepino", 4, 14, false, "18/01/2004", 700);
         // Agrega las bicicletas al almacén
-        almacen.agregarBicicletas(bici);
+        almacen.agregarNuevasBicicletas(nuevaBicicleta);
 
         // Imprime la tabla después de agregar las bicicletas
-        AlmacenBicis.imprimirTabla(almacen.getBicicletas());
+        AlmacenBicis.imprimirTabla(bicicletas);
 
+        int opcion;
 
         do {
             System.out.println("*************************\n" +
@@ -37,8 +36,7 @@ public class Tienda {
 
             switch (opcion) {
                 case 1:
-
-                    almacen.addBicicleta();
+                    almacen.agregarBicicleta();
                     break;
 
                 case 2:
@@ -51,7 +49,7 @@ public class Tienda {
 
                 case 4:
                     almacen.mostrarStockTotal();
-                    AlmacenBicis.imprimirTabla(AlmacenBicis.bicicletas);
+                    AlmacenBicis.imprimirTabla(bicicletas);
                     break;
             }
         } while (opcion != 0);
